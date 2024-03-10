9822b35d3a71f7843273bb16c567274a2dc6c396
bibhutiswain9 <bibhutiswain990@gmail.com>
2024-03-10 13:09:07 +0530

    add icons and react native element

--- apps/native/src/screens/Home/index.tsx
+++ apps/native/src/screens/Home/index.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import { Button } from "@repo/ui";
 import { push } from "../../utils/navigation";
 import { Routes } from "../../navigation/routes";
-
+import Icon from "react-native-vector-icons/Feather";
 export default function HomeScreen() {
   return (
     <View>
+      <Icon name="plus" size={30} />
       <Button
         text="Navigate to add task"
         onClick={() => push({ screenName: Routes.AddTask })}
